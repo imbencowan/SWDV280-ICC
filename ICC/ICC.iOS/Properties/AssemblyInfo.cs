4df475047eaa61b0388952ba54eaeb6c1a004e22
David <31709711+Macenstine@users.noreply.github.com>
2018-11-27 17:37:11 -0700

    Added some stuff to play with flashlight.

--- ICC/ICC.iOS/Properties/AssemblyInfo.cs
+++ ICC/ICC.iOS/Properties/AssemblyInfo.cs
@@ -34,3 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+//For Flashlight Demo Stuff
+[assembly: UsesPermission(Android.Manifest.Permission.Flashlight)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
